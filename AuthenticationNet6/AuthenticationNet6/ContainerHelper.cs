3a4db60f7e2b5e5620c572f34e1611c45cb98a79
David C <david.contreras@gorillalogic.com>
2022-10-30 12:57:01 -0600

    specify migrations assembly

--- AuthenticationNet6/AuthenticationNet6/ContainerHelper.cs
+++ AuthenticationNet6/AuthenticationNet6/ContainerHelper.cs
@@ -5,22 +5,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AuthenticationNet6
 {
-    public static class ContainerHelper
-    {
-        public static void RegisterServices(IServiceCollection services, ConfigurationManager configuration)
-        {
-            services.AddScoped<IAppUserRepository, UserRepository>();
-            services.AddScoped<IUnitiyOfWork, UnitOfWork>();
-            services.AddScoped<IUserService, UserService>();
-            SetupEntityFramework(services, configuration);
-        }
+	public static class ContainerHelper
+	{
+		public static void RegisterServices(IServiceCollection services, ConfigurationManager configuration)
+		{
+			services.AddScoped<IAppUserRepository, UserRepository>();
+			services.AddScoped<IUnitiyOfWork, UnitOfWork>();
+			services.AddScoped<IUserService, UserService>();
+			SetupEntityFramework(services, configuration);
+		}
 
-        private static void SetupEntityFramework(IServiceCollection services, ConfigurationManager configuration)
-        {
+		private static void SetupEntityFramework(IServiceCollection services, ConfigurationManager configuration)
+		{
 			services.AddDbContext<EFAppContext>(options =>
 				  options.UseSqlServer(configuration.GetConnectionString("SQLServer")
-									  //,x => x.MigrationsAssembly("Your.Migrations")
-                                      ));
+									  , x => x.MigrationsAssembly("EFDataAccess")
+									  ));
 		}
-    }
+	}
 }
